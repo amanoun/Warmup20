7339aab5ac7fed633ac986acc7877aac56e66a81
Anas <anas.manoun@aa.com>
2018-07-24 13:40:11 -0500

    added a test was deleted by accident

--- src/test/java/warmup2/StringNonStartTest.java
+++ src/test/java/warmup2/StringNonStartTest.java
@@ -1,5 +1,24 @@
+package warmup2;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StringNonStartTest {
 
+        StringNonStart stringNonStart = new StringNonStart();
+    @Test
+    public void nonStartShouldReturnellohere() {
+        assertEquals( "ellohere", stringNonStart.nonStart("Hello", "There"));
+    }
+
+    @Test
+    public void nonStartShouldReturnavaode() {
+        assertEquals( "avaode", stringNonStart.nonStart("java", "code"));
+    }
+
+    @Test
+    public void nonStartShouldReturnAnEmptyString() {
+        assertEquals( "", stringNonStart.nonStart("a", "b"));
+    }
 }
