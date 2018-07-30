c0f815308c081a80f60001fb2af0565c2e6cd2f8
Anas <anas.manoun@aa.com>
2018-07-30 14:50:51 -0500

    Refactored String2 EndOther

--- src/main/java/warmup2/String2EndOther.java
+++ src/main/java/warmup2/String2EndOther.java
@@ -3,13 +3,8 @@ package warmup2;
 public class String2EndOther {
     public boolean endOther(String a, String b) {
 
-        if (a.toLowerCase().endsWith(b.toLowerCase())
-                || b.toLowerCase().endsWith(a.toLowerCase())) {
-
-            return true;
-
-        }
-        return false;
+        return (a.toLowerCase().endsWith(b.toLowerCase())
+                || b.toLowerCase().endsWith(a.toLowerCase()));
 
     }
 }
