bedb81be6b9b260c23f8759e05db021c3468fb86
Anas <anas.manoun@aa.com>
2018-07-31 10:48:54 -0500

    refactored String2 BobThere and Used Rgex Yeehhaaaaaaaa!

--- src/main/java/warmup2/String2BobThere.java
+++ src/main/java/warmup2/String2BobThere.java
@@ -1,26 +1,27 @@
 package warmup2;
 
-class String2BobThere {
+import java.util.regex.Pattern;
 
+class String2BobThere {
     boolean bobThere(String str) {
 
-        int length = str.length();
-
-        if (length <= 2) {
-            return false;
-        }
-
-
-        for (int i = 0; i < length - 1; i++) {
-
-            if ("b".equals(Character.toString(str.charAt(i))) && i + 2 < length &&
-                    "b".equals(Character.toString(str.charAt(i + 2)))) {
-
-                return true;
-            }
-
+        Pattern p = Pattern.compile("b.b");   // the pattern to search for
+        return p.matcher(str).find();
 
         }
-        return false;
     }
-}
+
+
+//        for (int i = 0; i < length - 1; i++) {
+//
+//            if ("b".equals(Character.toString(str.charAt(i))) && i + 2 < length &&
+//                    "b".equals(Character.toString(str.charAt(i + 2)))) {
+//
+//                return true;
+//            }
+//
+//
+//        }
+//        return false;
+//    }
+
