7ec5f0e145c5b42a4a3346ccb2e7aa85ed73f5ae
Anas <anas.manoun@aa.com>
2018-08-01 13:34:48 -0500

    refactored String2 GeStandwish after reading online and learning first and last index of yeeehaa!

--- src/main/java/warmup2/String2GetSandwish.java
+++ src/main/java/warmup2/String2GetSandwish.java
@@ -3,26 +3,37 @@ package warmup2;
 class String2GetSandwish {
     String getSandwish(String str) {
 
-        int begin = 0;
-        int end = 0;
+        int begin = str.indexOf("bread");
+        int end = str.lastIndexOf("bread");
+        if ((begin != end) && (end != -1)) {
 
-        for(int i = 0; i < str.length() - 5; i++) {
-            if(str.substring(i, i + 5).equals("bread")) {
-                begin = i;
-                break;
-            }
-        }
-
-        for(int i = str.length() - 5; i >= 0; i--) {
-            if(str.substring(i, i + 5).equals("bread")) {
-                end = i;
-                break;
-            }
-        }
-
-        if( begin != end)
             return str.substring(begin + 5, end);
-
+        }
         return "";
     }
 }
+
+
+//        int begin = 0;
+//        int end = 0;
+//
+//        for (int i = 0; i < str.length() - 5; i++) {
+//            if (str.substring(i, i + 5).equals("bread")) {
+//                begin = i;
+//                break;
+//            }
+//        }
+//
+//        for (int i = str.length() - 5; i >= 0; i--) {
+//            if (str.substring(i, i + 5).equals("bread")) {
+//                end = i;
+//                break;
+//            }
+//        }
+//
+//        if (begin != end)
+//            return str.substring(begin + 5, end);
+//
+//        return "";
+//    }
+//}
