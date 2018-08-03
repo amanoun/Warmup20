7f3e3205e78235511c8aed977650406ac840ed32
Anas <anas.manoun@aa.com>
2018-08-03 15:02:22 -0500

    added  Array2 CountEvens challenge and tests

--- src/main/java/warmup2/Array2CountEvens.java
+++ src/main/java/warmup2/Array2CountEvens.java
@@ -1,13 +1,18 @@
 package warmup2;
 
+
 class Array2CountEvens {
     int countEvens(int[] nums) {
         int count = 0;
-        for (int i = 0; i < nums.length; i++)
-            if (nums[i] % 2 == 0) {
+
+        for (int i : nums) {
+            if (i % 2 == 0) {
                 count++;
+
             }
+        }
         return count;
     }
+
 }
 
